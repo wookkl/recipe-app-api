58eba640b99d10fcc081ab81695eb44402504e82
wookkl <wjddnr315@gmail.com>
2021-01-11 00:27:48 +0900

    Refactored tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,37 +6,30 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """ Manage tags in the database """
+class BaseRecipeAttr(viewsets.GenericViewSet,
+                     mixins.ListModelMixin,
+                     mixins.CreateModelMixin):
+    """ Base viewset for user owned recipe attributes """
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """ Return objects for the current authenticated user only """
         return self.queryset.filter(user=self.request.user).order_by("-name")
 
     def perform_create(self, serializer):
-        """ Create a new tag """
+        """ Create a new object """
         serializer.save(user=self.request.user)
 
 
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class TagViewSet(BaseRecipeAttr):
+    """ Manage tags in the database """
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttr):
     """ Manage ingredients in the database """
 
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
-    serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by("-name")
-
-    def perform_create(self, serializer):
-        """ Create a new ingredient """
-        serializer.save(user=self.request.user)
+    serializer_class = serializers.IngredientSerializer
