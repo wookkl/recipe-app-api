228108733de18405b76cd20fe6b5856b039cb5cb
wookkl <wjddnr315@gmail.com>
2021-01-12 00:48:53 +0900

    Added feature to filter tags and ingredients

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Tag
+from core.models import Tag, Recipe
 
 from recipe.serializers import TagSerializer
 
@@ -80,3 +80,47 @@ class PrivateTagsApiTests(TestCase):
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_tags_assigned_to_recipes(self):
+        """ Test filtering tags by those  assigned to recipes """
+        tag1 = Tag.objects.create(user=self.user, name="Breakfast")
+        tag2 = Tag.objects.create(user=self.user, name="Lunch")
+        recipe = Recipe.objects.create(
+            user=self.user,
+            title="Toast",
+            time_minute=20,
+            price=5.00
+        )
+        recipe.tags.add(tag1)
+
+        res = self.client.get(TAGS_URL, {"assigned_only": 1})
+
+        serializer1 = TagSerializer(tag1)
+        serializer2 = TagSerializer(tag2)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertNotIn(serializer2.data, res.data)
+
+    def test_retrieve_tags_assigned_unique(self):
+        """ Test filtering tags by assigned returnsunique items """
+
+        tag = Tag.objects.create(user=self.user, name="Breakfast")
+        Tag.objects.create(user=self.user, name="Lunch")
+        recipe1 = Recipe.objects.create(
+            user=self.user,
+            title="Pancakes",
+            time_minute=15,
+            price=4.00
+        )
+        recipe1.tags.add(tag)
+        recipe2 = Recipe.objects.create(
+            user=self.user,
+            title="Bread",
+            time_minute=3,
+            price=1.00
+        )
+
+        recipe2.tags.add(tag)
+        res = self.client.get(TAGS_URL, {"assigned_only": 1})
+
+        self.assertEqual(len(res.data), 1)
